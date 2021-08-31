c14e5d3969021931cf8641d5b9246b5faab4b59f
billy <iago.siqueiralima@mail.com>
2021-08-30 21:15:44 -0300

    AtividadeLogatti

---
+++ ProjetoAsPCore/ProjetoAsPCore/Data/ProjetoAsPCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoAsPCore.Model;
+
+namespace ProjetoAsPCore.Data
+{
+    public class ProjetoAsPCoreContext : DbContext
+    {
+        public ProjetoAsPCoreContext (DbContextOptions<ProjetoAsPCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoAsPCore.Model.Person> Person { get; set; }
+    }
+}
